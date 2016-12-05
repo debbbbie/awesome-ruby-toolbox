d32a8132ec0437100454d086b9163ad9daec7a97
debbbbie <debbbbie@163.com>
2016-12-05 08:24:08 +0800

    add awesome logo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Awesome Ruby Toolbox
+# Awesome Ruby Toolbox [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
 
 A collection of awesome Ruby libraries from https://www.ruby-toolbox.com , with removing `DRPRECATED` libraries, thanks the author colszowka!
 
--- lib/awesome_ruby_toolbox.rb
+++ lib/awesome_ruby_toolbox.rb
@@ -111,7 +111,7 @@ module AwesomeRubyToolbox
     end.flatten.join("\n")
 
     ret = <<-README
-#{title}
+#{title} [![Awesome](https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)](https://github.com/sindresorhus/awesome)
 
 A collection of awesome Ruby libraries from https://www.ruby-toolbox.com , with removing `DRPRECATED` libraries, thanks the author colszowka!
 
